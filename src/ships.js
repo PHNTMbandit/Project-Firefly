ccafda36870bd9d83ef0163e0df59bcd4d4e2269
Domenic Pittari <dom.pittari@gmail.com>
2023-12-05 21:28:39 +0900

    added left and right animations

--- src/ships.js
+++ src/ships.js
@@ -4,6 +4,8 @@ var getShip = function (shipName) {
   const ships = {
     Proto: {
       sprite: "8x8/SpaceShooterAssetPack_Ships-1.png",
+      leftTurnSprite: "8x8/SpaceShooterAssetPack_Ships-2.png",
+      rightTurnSprite: "8x8/SpaceShooterAssetPack_Ships-0.png",
       projectileType: "laser",
       health: 100,
       speed: 65,
@@ -20,6 +22,8 @@ export default function spawnShip(shipName, scene, x, y) {
     x,
     y,
     ship.sprite,
+    ship.leftTurnSprite,
+    ship.rightTurnSprite,
     ship.projectileType,
     ship.health,
     ship.speed
@@ -27,16 +31,51 @@ export default function spawnShip(shipName, scene, x, y) {
 }
 
 class Ship extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene, x, y, sprite, projectileType, health, speed) {
+  constructor(
+    scene,
+    x,
+    y,
+    sprite,
+    leftTurnSprite,
+    rightTurnSprite,
+    projectileType,
+    health,
+    speed
+  ) {
     super(scene, x, y, "ships", sprite);
 
     scene.add.existing(this);
     scene.physics.add.existing(this);
 
+    this.leftTurnSprite = leftTurnSprite;
+    this.rightTurnSprite = rightTurnSprite;
     this.projectileGroup = spawnProjectileGroup(projectileType, scene);
     this.health = health;
     this.speed = speed;
     this.setCollideWorldBounds(true);
+
+    this.scene.anims.create({
+      key: "left",
+      frames: [{ key: "ships", frame: leftTurnSprite }],
+      frameRate: 1,
+      repeat: -1,
+    });
+
+    this.scene.anims.create({
+      key: "right",
+      frames: [{ key: "ships", frame: rightTurnSprite }],
+      frameRate: 1,
+      repeat: -1,
+    });
+
+    this.scene.anims.create({
+      key: "straight",
+      frames: [
+        { key: "ships", frame: "8x8/SpaceShooterAssetPack_Ships-1.png" },
+      ],
+      frameRate: 1,
+      repeat: -1,
+    });
   }
 
   takeDamage(amount) {
