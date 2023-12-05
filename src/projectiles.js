79d9a21565dbedca4574ad5bb04d4db266d96bfd
Domenic Pittari <dom.pittari@gmail.com>
2023-12-05 20:27:56 +0900

    added damage functions

--- src/projectiles.js
+++ src/projectiles.js
@@ -2,6 +2,7 @@ var getProjectileType = function (projectileTypeName) {
   const types = {
     laser: {
       sprite: "SpaceShooterAssetPack_Projectiles-0.png",
+      damage: 10,
       fireRate: 100,
       amount: 100,
       scale: 1,
@@ -16,17 +17,20 @@ export default function spawnProjectileGroup(projectileTypeName, scene) {
   return new ProjectileGroup(
     scene,
     projectileType.sprite,
+    projectileType.damage,
     projectileType.fireRate,
     projectileType.amount,
     projectileType.scale
   );
 }
 
+let projectileDamage;
 class ProjectileGroup extends Phaser.Physics.Arcade.Group {
-  constructor(scene, sprite, fireRate, amount, scale) {
+  constructor(scene, sprite, damage, fireRate, amount, scale) {
     super(scene.physics.world, scene);
 
     this.sprite = sprite;
+    projectileDamage = damage;
     this.fireRate = fireRate;
     this.fireElapsedTime = 0;
 
@@ -51,6 +55,11 @@ class ProjectileGroup extends Phaser.Physics.Arcade.Group {
       this.fireElapsedTime = time + this.fireRate;
       projectile.shoot(x, y);
     }
+  }
+
+  damageTarget(target, projectile) {
+    target.takeDamage(projectileDamage);
+    projectile.disableBody(true, true);
   }
 }
 
--- src/ships.js
+++ src/ships.js
@@ -4,8 +4,9 @@ var getShip = function (shipName) {
   const ships = {
     Proto: {
       sprite: "8x8/SpaceShooterAssetPack_Ships-1.png",
-      speed: 65,
       projectileType: "laser",
+      health: 100,
+      speed: 65,
     },
   };
 
@@ -14,18 +15,35 @@ var getShip = function (shipName) {
 
 export default function spawnShip(shipName, scene, x, y) {
   const ship = getShip(shipName);
-  return new Ship(scene, x, y, ship.sprite, ship.speed, ship.projectileType);
+  return new Ship(
+    scene,
+    x,
+    y,
+    ship.sprite,
+    ship.projectileType,
+    ship.health,
+    ship.speed
+  );
 }
 
 class Ship extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene, x, y, sprite, speed, projectileType) {
+  constructor(scene, x, y, sprite, projectileType, health, speed) {
     super(scene, x, y, "ships", sprite);
 
     scene.add.existing(this);
     scene.physics.add.existing(this);
 
-    this.speed = speed;
     this.projectileGroup = spawnProjectileGroup(projectileType, scene);
+    this.health = health;
+    this.speed = speed;
     this.setCollideWorldBounds(true);
+  }
+
+  takeDamage(amount) {
+    this.health -= amount;
+
+    if (this.health <= 0) {
+      this.disableBody(true, true);
+    }
   }
 }
