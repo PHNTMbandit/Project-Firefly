f63537b8eeaaf67a906ebfdd5d80487dee05148a
Domenic Pittari <dom.pittari@gmail.com>
2023-12-16 16:37:43 +0900

    added abstraction to projectiles

--- src/projectiles.js
+++ src/projectiles.js
@@ -1,36 +1,55 @@
 export var getProjectile = function (name) {
   const projectiles = {
     "Auto Cannon Bullet": {
-      class: AutoCannonBullet,
+      projectileType: Projectile,
       damage: 5,
       fireRate: 200,
       speed: 350,
       spriteSheet: "projectiles",
+      sprite: "Bullet/Main ship weapon - Projectile - Auto cannon bullet-0",
+      frames: 3,
     },
     "Energy Ball": {
-      class: EnergyBall,
+      projectileType: Projectile,
       damage: 10,
       speed: 350,
       spriteSheet: "projectiles",
+      sprite: "Energy Ball/Main ship weapon - Projectile - Big Space Gun-0",
+      frames: 9,
     },
     "Laser Beam": {
-      class: LaserBeam,
+      projectileType: Projectile,
       damage: 10,
       spriteSheet: "projectiles",
+      sprite: "Laser Beam/Main ship weapon - Projectile - Zapper-0",
+      frames: 7,
     },
     Rocket: {
-      class: Rocket,
+      projectileType: Projectile,
       damage: 5,
       fireRate: 600,
       speed: 350,
       spriteSheet: "projectiles",
+      sprite: "Rocket/Main ship weapon - Projectile - Rocket-0",
+      frames: 2,
     },
     "Vaxtra Bullet": {
-      class: VaxtraBullet,
+      projectileType: Projectile,
       damage: 5,
       fireRate: 700,
       speed: 500,
       spriteSheet: "Kla'ed",
+      sprite: "Projectiles/Bullet/Kla'ed - Bullet-0",
+      frames: 7,
+    },
+    "Vaxtra Big Bullet": {
+      projectileType: Projectile,
+      damage: 5,
+      fireRate: 700,
+      speed: 500,
+      spriteSheet: "Kla'ed",
+      sprite: "Projectiles/Big Bullet/Kla'ed - Big Bullet-0",
+      frames: 3,
     },
   };
 
@@ -41,15 +60,28 @@ export default class ProjectileGroup extends Phaser.Physics.Arcade.Group {
   constructor(scene, projectile, amount) {
     super(scene.physics.world, scene);
 
+    this.projectile = projectile;
+
     this.createMultiple({
-      classType: projectile.class,
+      classType: projectile.projectileType,
       key: projectile.spriteSheet,
+      frame: projectile.sprite,
       frameQuantity: amount,
       active: false,
       visible: false,
     });
 
-    this.projectile = projectile;
+    scene.anims.create({
+      key: `${projectile.sprite} shoot`,
+      frames: scene.anims.generateFrameNames(projectile.spriteSheet, {
+        prefix: projectile.sprite.slice(0, -1),
+        end: projectile.frames,
+        zeroPad: 1,
+      }),
+      frameRate: 15,
+      repeat: -1,
+      showOnStart: true,
+    });
   }
 
   getProjectile(index) {
@@ -62,8 +94,9 @@ export default class ProjectileGroup extends Phaser.Physics.Arcade.Group {
 
   shootProjectile(x, y, direction, scale) {
     const projectile = this.getFirstDead(false);
+    projectile.data = this.projectile;
+    this.scene.anims.play(`${projectile.data.sprite} shoot`, projectile);
     projectile.setScale(scale);
-    projectile.anims.play("shoot");
     projectile.enableBody(true, x, y, true, true);
 
     if (direction == "up") {
@@ -75,160 +108,14 @@ export default class ProjectileGroup extends Phaser.Physics.Arcade.Group {
   }
 
   dealDamage(target, projectile) {
-    const projectileName = projectile.constructor.name.replace(
-      /([a-z])([A-Z])/g,
-      "$1 $2"
-    );
-
-    target.last.takeDamage(
-      getProjectile(projectileName).damage * projectile.scale
-    );
+    target.last.takeDamage(projectile.data.damage * projectile.scale);
     projectile.disableBody(true, true);
   }
 }
 
-class AutoCannonBullet extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene, x, y) {
-    super(
-      scene,
-      x,
-      y,
-      "projectiles",
-      "Bullet/Main ship weapon - Projectile - Auto cannon bullet-0"
-    );
-
-    this.anims.create({
-      key: "shoot",
-      frames: this.anims.generateFrameNames("projectiles", {
-        prefix: "Bullet/Main ship weapon - Projectile - Auto cannon bullet-",
-        end: 3,
-        zeroPad: 1,
-      }),
-      frameRate: 15,
-      repeat: -1,
-      showOnStart: true,
-    });
-
-    scene.physics.add.existing(this);
-    this.body.setSize(this.frame.cutWidth, this.frame.cutHeight);
-  }
-
-  preUpdate(time, delta) {
-    super.preUpdate(time, delta);
-
-    if (this.y <= 0) {
-      this.disableBody(true, true);
-    }
-  }
-}
-
-class EnergyBall extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene, x, y) {
-    super(
-      scene,
-      x,
-      y,
-      "projectiles",
-      "Energy Ball/Main ship weapon - Projectile - Big Space Gun-0"
-    );
-
-    this.anims.create({
-      key: "shoot",
-      frames: this.anims.generateFrameNames("projectiles", {
-        prefix: "Energy Ball/Main ship weapon - Projectile - Big Space Gun-",
-        end: 9,
-        zeroPad: 1,
-      }),
-      frameRate: 15,
-      repeat: -1,
-      showOnStart: true,
-    });
-
-    scene.physics.add.existing(this);
-    this.body.setSize(this.frame.cutWidth, this.frame.cutHeight);
-  }
-
-  preUpdate(time, delta) {
-    super.preUpdate(time, delta);
-
-    if (this.y <= 0) {
-      this.disableBody(true, true);
-    }
-  }
-}
-
-class LaserBeam extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene, x, y) {
-    super(
-      scene,
-      x,
-      y,
-      "projectiles",
-      "Laser Beam/Main ship weapon - Projectile - Zapper-0"
-    );
-
-    this.anims.create({
-      key: "shoot",
-      frames: this.anims.generateFrameNames("projectiles", {
-        prefix: "Laser Beam/Main ship weapon - Projectile - Zapper-",
-        end: 7,
-        zeroPad: 1,
-      }),
-      frameRate: 15,
-    });
-  }
-}
-
-class Rocket extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene, x, y) {
-    super(
-      scene,
-      x,
-      y,
-      "projectiles",
-      "Rocket/Main ship weapon - Projectile - Rocket-0"
-    );
-
-    this.anims.create({
-      key: "shoot",
-      frames: this.anims.generateFrameNames("projectiles", {
-        prefix: "Rocket/Main ship weapon - Projectile - Rocket-",
-        end: 2,
-        zeroPad: 1,
-      }),
-      frameRate: 15,
-      repeat: -1,
-      showOnStart: true,
-    });
-
-    scene.physics.add.existing(this);
-    this.body.setSize(this.frame.cutWidth, this.frame.cutHeight);
-  }
-
-  preUpdate(time, delta) {
-    super.preUpdate(time, delta);
-
-    if (this.y <= 0) {
-      this.disableBody(true, true);
-    }
-  }
-}
-
-class VaxtraBullet extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene, x, y) {
-    super(scene, x, y, "Kla'ed", "Projectiles/Bullet/Kla'ed - Bullet-0");
-
-    this.anims.create({
-      key: "shoot",
-      frames: this.anims.generateFrameNames("Kla'ed", {
-        prefix: "Projectiles/Bullet/Kla'ed - Bullet-",
-        end: 7,
-        zeroPad: 1,
-      }),
-      frameRate: 15,
-      repeat: -1,
-      showOnStart: true,
-    });
+class Projectile extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y, spriteSheet, spriteName) {
+    super(scene, x, y, spriteSheet, spriteName);
 
     scene.physics.add.existing(this);
     this.body.setSize(this.frame.cutWidth, this.frame.cutHeight);
@@ -240,5 +127,11 @@ class VaxtraBullet extends Phaser.Physics.Arcade.Sprite {
     if (this.y <= 0 || this.y >= this.scene.scale.height + 50) {
       this.disableBody(true, true);
     }
+  }
+}
+
+class Beam extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y, spriteSheet, spriteName) {
+    super(scene, x, y, spriteSheet, spriteName);
   }
 }
