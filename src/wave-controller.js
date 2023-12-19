894ef39aeae2d73d03c411ae7f66f29e7a0a4389
Domenic Pittari <dom.pittari@gmail.com>
2023-12-19 15:32:33 +0900

    added triangle formation

--- src/wave-controller.js
+++ src/wave-controller.js
@@ -23,45 +23,42 @@ export default class WaveController {
     );
 
     scene.time.addEvent({
-      callback: () => this.spawnGridFormation(3, 1),
-      delay: 2500,
+      callback: () => this.spawnTriangle(4),
+      delay: 4000,
       loop: true,
     });
   }
 
-  spawnOnHorizon(scene) {
-    const bounds = this.scene.physics.world.bounds;
-    const positionX = Phaser.Math.Between(bounds.x, bounds.x + bounds.width);
-    const enemy = this.enemies.spawnShip(positionX, bounds.y - 32);
-
-    this.tween = scene.tweens.add({
-      targets: enemy.body.velocity,
-      x: 50,
-      duration: 2000,
-      ease: "Cubic.easeInOut",
-      repeat: -1,
-      yoyo: true,
-      loop: true,
-    });
-  }
-
-  spawnGridFormation(gridX, gridY) {
+  spawnGrid(gridX, gridY) {
     const defaultSpacing = 15;
     const bounds = this.scene.physics.world.bounds;
     const spacingX = this.enemies.getFirst().body.width + defaultSpacing;
     const spacingY = this.enemies.getFirst().body.height + defaultSpacing;
-    const offsetY = bounds.y - spacingY;
     const minOffsetX = bounds.x + spacingX;
     const maxOffsetX = bounds.width - (gridX - 1) * spacingX - spacingX;
     const offsetX = Phaser.Math.FloatBetween(minOffsetX, maxOffsetX);
+    const offsetY = bounds.y - spacingY * gridY;
 
     for (let y = 0; y < gridY; y++) {
       for (let x = 0; x < gridX; x++) {
-        const enemy = this.enemies.spawnShip(
-          offsetX + x * spacingX,
-          offsetY + y * spacingY
-        );
-        console.log(enemy.health);
+        this.enemies.spawnShip(offsetX + x * spacingX, offsetY + y * spacingY);
+      }
+    }
+  }
+
+  spawnTriangle(gridSize) {
+    const defaultSpacing = 15;
+    const bounds = this.scene.physics.world.bounds;
+    const spacingX = this.enemies.getFirst().body.width + defaultSpacing;
+    const spacingY = this.enemies.getFirst().body.height + defaultSpacing;
+    const minOffsetX = bounds.x + spacingX;
+    const maxOffsetX = bounds.width - spacingX * (gridSize * 2 - 1);
+    const offsetX = Phaser.Math.FloatBetween(minOffsetX, maxOffsetX);
+    const offsetY = bounds.y - spacingY * gridSize;
+
+    for (let y = 0; y < gridSize; y++) {
+      for (let x = y; x < 2 * gridSize - y - 1; x++) {
+        this.enemies.spawnShip(offsetX + x * spacingX, offsetY + y * spacingY);
       }
     }
   }
